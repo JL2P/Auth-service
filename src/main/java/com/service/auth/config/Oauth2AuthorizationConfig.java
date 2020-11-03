3c4d482b73dc3ff8b8789e175c6935f0ebfda8ff
shoon2430 <shoon2430@gmail.com>
2020-11-03 13:34:29 +0900

    JWT토큰으로 변경

--- src/main/java/com/service/auth/config/Oauth2AuthorizationConfig.java
+++ src/main/java/com/service/auth/config/Oauth2AuthorizationConfig.java
@@ -1,11 +1,16 @@
 package com.service.auth.config;
 
+import com.service.auth.domain.service.CustomUserDetailService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 import javax.sql.DataSource;
 
@@ -16,16 +21,12 @@ public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
 
     private final PasswordEncoder passwordEncoder;
     private final DataSource dataSource;
+    private final CustomUserDetailService userDetailService;
 
-//    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-//        clients.inMemory()
-//                .withClient("testClientId")
-//                .secret("{noop}testSecret")
-//                .redirectUris("http://localhost:9000/oauth2/callback")
-//                .authorizedGrantTypes("authorization_code")
-//                .scopes("read", "write")
-//                .accessTokenValiditySeconds(30000);
-//    }
+    @Value("${security.oauth2.jwt.signkey}")
+    private String signKey;
+
+
     /**
      * 클라이언트 정보 주입 방식을 jdbcdetail로 변경
      *
@@ -35,5 +36,40 @@ public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.jdbc(dataSource).passwordEncoder(passwordEncoder);
+    }
+
+    /**
+     * 토큰 발급 방식을 JWT 토큰 방식으로 변경한다. 이렇게 하면 토큰 저장하는 DB Table은 필요가 없다.
+     *
+     * @param endpoints
+     * @throws Exception
+     */
+//    @Override
+//    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+//        super.configure(endpoints);
+//        endpoints.accessTokenConverter(jwtAccessTokenConverter());
+//    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        super.configure(endpoints);
+        endpoints.accessTokenConverter(jwtAccessTokenConverter()).userDetailsService(userDetailService);
+    }
+
+
+    /**
+     * jwt converter를 등록
+     *
+     * @return
+     */
+//    @Bean public JwtAccessTokenConverter jwtAccessTokenConverter() {
+//        return new JwtAccessTokenConverter();
+//    }
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        // jwt 키 설정
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(signKey);
+        return converter;
     }
 }
